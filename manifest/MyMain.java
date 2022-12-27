3c9cdc6a68a228592013352ef6898154b113fd5a
Vit Gottwald <vit.gottwald@gmail.com>
2022-12-27 15:45:30 +0100

    Initial commit

---
+++ manifest/MyMain.java
@@ -0,0 +1,7 @@
+package com.mypackage;
+
+class MyMain {
+  public static void main(String[] args) {
+    System.out.println("Hi");
+  }
+}
---
+++ single/Single.java
@@ -0,0 +1,5 @@
+class Single {
+  public static void main(String[] args) {
+    System.out.println("Hi");
+  }
+}
